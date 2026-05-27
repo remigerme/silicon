98fcd53056e580a397448dc152b5da4948822540
remi.germe <remi.germe@gmail.com>
2026-05-27 17:16:27 +0200

    Revert previous commit as the issue also exists for unfolding expressions

--- src/main/scala/rules/MagicWandSupporter.scala
+++ src/main/scala/rules/MagicWandSupporter.scala
@@ -583,12 +583,6 @@ object magicWandSupporter extends SymbolicExecutionRules {
       s.h
   }
 
-  def getAttachedEvalHeap(s: State): Heap = {
-    // See the in_package.vpr test case of attached facts for more details.
-    if (s.exhaleExt) s.reserveHeaps.foldLeft(s.h)(_ + _)
-    else s.h
-  }
-
   def getExecutionHeap(s: State): Heap =
     if (s.exhaleExt) s.reserveHeaps.head
     else s.h
